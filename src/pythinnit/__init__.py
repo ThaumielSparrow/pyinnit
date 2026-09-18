2855a966191a25c54f996b650acb26f95d480cfb
ThaumielSparrow <lzhang1337@gmail.com>
2026-09-18 11:24:46 -0400

    chewsday

---
+++ src/pythinnit/__init__.py
@@ -0,0 +1,3 @@
+from ._patch import install
+
+install()
---
+++ src/pythinnit/_patch.py
@@ -0,0 +1,23 @@
+import builtins
+
+
+_original_build_class = builtins.__build_class__
+
+
+def _british_build_class(func, name, *bases, **kwargs):
+    cls = _original_build_class(func, name, *bases, **kwargs)
+
+    namespace = cls.__dict__
+
+    if "__innit__" in namespace and "__init__" not in namespace:
+        cls.__init__ = namespace["__innit__"]
+
+    return cls
+
+
+def install():
+    if getattr(builtins.__build_class__, "__pythinnit__", False):
+        return
+
+    _british_build_class.__pythinnit__ = True
+    builtins.__build_class__ = _british_build_class
